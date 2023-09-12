d13518ecca84969f7d895d363082067b305bfa82
sebmaz <seb.maz93@icloud.com>
2023-09-13 02:50:00 +0400

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def take_input():
+    # name = input("whats your name ? \n")
+    # age = input("whats your age ?\n")
+    num = 123
+    flt = 1.4
+    num_flt = num + flt
+    num_str = str(num)
+    # print(f"Hello, {name} you are {age} year's old")
+    # print("Hello " + name + " age of " + age + " " + num_str)
+    print(num_flt)
+
+
+# check type using type()
+# convert string to int  using int() and vise versa using str() same for float()
+# num[2] gives 3 but string type same works for strings[0]
+
+# dividing numbers gives back FLOAT type using // gives non-float
+# order or mathematical operations
+# PEMDAS  () __ ** __ * / __ + -   (if from same group the left one will be executed first)
+# round(num, decimal) to round a number
+#
+
+if __name__ == "__main__":
+    take_input()
